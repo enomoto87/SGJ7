0ed6027746f2a2369813cbd172821936fce4f034
Enomoto3 <cito18002@g.nihon-u.ac.jp>
2018-12-11 16:44:29 +0900

    銃口から弾が出るように修正した．ただし、照準がクソ

--- Assets/VR_FPS/Scripts/Aim.cs
+++ Assets/VR_FPS/Scripts/Aim.cs
@@ -11,6 +11,8 @@ public class Aim : MonoBehaviour {
     GameObject bullet;
     [SerializeField]
     GameObject cameraEye;
+    [SerializeField]
+    GameObject juukou;
     float interval = 0f;
 
 	// Use this for initialization
@@ -26,7 +28,7 @@ public class Aim : MonoBehaviour {
         // rayのあたり判定の情報を入れる箱を作る。
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 60))
+        if (Physics.Raycast(ray, out hit, 60, 1<<8))
         {
 
             string hitName = hit.transform.gameObject.tag;
@@ -34,12 +36,18 @@ public class Aim : MonoBehaviour {
             if (hitName == "Enemy")
             {
                 Debug.Log(hit.point.x.ToString());//オブジェクトとの接触座標
-                //Debug.Log(hit.transform.localPosition.x.ToString());//当たったオブジェクトの座標取得
+                Debug.Log(hit.transform.localPosition.x.ToString());//当たったオブジェクトの座標取得
                 // 照準器の色を「赤」に変える（色は自由に変更してください。）
                 aimImage.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
                 if(this.interval <= 0f)
                 {
-                    Instantiate(this.bullet, this.cameraEye.transform.position, this.cameraEye.transform.rotation * Quaternion.Euler(-90f, 180f, 0f));
+                    var heading = hit.point - this.juukou.transform.position;
+                    var distance = heading.magnitude;
+                    var direction = heading / distance;
+                    Debug.DrawLine(this.juukou.transform.position, heading);
+
+                    Instantiate(this.bullet, this.juukou.transform.position, this.cameraEye.transform.rotation * Quaternion.Euler(-90f, 180f, 0f));
+                    //Instantiate(this.bullet, this.juukou.transform.position, Quaternion.Euler(direction) * Quaternion.Euler(-90f, 180f, 0f));
                     this.interval = 60f;
                 }         
                     
