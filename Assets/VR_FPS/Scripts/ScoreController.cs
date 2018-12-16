3cc37e585eb43910a93688c1387498fb41a1f069
Enomoto3 <cito18002@g.nihon-u.ac.jp>
2018-12-16 09:31:44 +0900

    ハイスコア作成中

---
+++ Assets/VR_FPS/Scripts/ScoreController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreController : MonoBehaviour {
+    [SerializeField]
+    Text scoreText;
+    
+
+	// Use this for initialization
+	void Start () {
+        int[] highScore = new int[] { 0, 0, 0 };
+        for (int i = 0; i < highScore.Length; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    Debug.Log("for error");
+                    break;
+            }
+        }
+
+        this.scoreText.text = "No1 : " + highScore[0].ToString() + 
+            "\nNo2 : " + highScore[1].ToString() + 
+            "\nNo3 : " + highScore[2].ToString() + 
+            "\n\nYour Point : " + Score.score.ToString(); 
+	}
+	
+	
+}
