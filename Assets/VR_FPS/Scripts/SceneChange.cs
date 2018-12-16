ae7608bffe5091c561cdb655c63a5eec430361b6
yakitoriman <ciwa18003@g.nihon-u.ac.jp>
2018-12-16 11:51:18 +0900

    ハイスコアの実装が完了

--- Assets/VR_FPS/Scripts/SceneChange.cs
+++ Assets/VR_FPS/Scripts/SceneChange.cs
@@ -10,7 +10,7 @@ public class SceneChange : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         time++;
-        if (time > 1800)
+        if (time > 3000)
         {
             SceneManager.LoadScene("gameover");
         }
